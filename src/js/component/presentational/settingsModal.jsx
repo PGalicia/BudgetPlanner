6b3e7109c2a1b5193dbabb6a7c54d318332e0850
PGalicia <galicia.patrick@gmail.com>
2019-06-16 16:12:12 -0700

    Settings Modal now validates

--- src/js/component/presentational/settingsModal.jsx
+++ src/js/component/presentational/settingsModal.jsx
@@ -38,13 +38,37 @@ class SettingsModal extends Component {
   }
 
   handlePercentageInputChange(e) {
-    // @todo Ensure that the value in this input is just a number between 0 and 100
-    this.setState({ percentage: e.target.value });
+    // Ensure that the state will only update if it's between 0 and 100
+    const percentage =
+      e.target.value === "" ? "" : Number.parseInt(e.target.value);
+
+    if (!isNaN(percentage) && percentage <= 100) {
+      this.setState({ percentage });
+    }
   }
 
   handleTotalMoneyInputChange(e) {
-    // @todo Ensure that the value in this input is just a number (ie valid money number)
+    // Remove all periods
+    let totalMoney =
+      e.target.value === "" ? "0" : e.target.value.replace(".", "");
+
+    // Limit the number to 7 character (including the period)
+    if (totalMoney.length > 7) {
+      return;
+    }
+    if (totalMoney.length > 2) {
+      // Place the period to mimick money format
+      totalMoney = totalMoney
+        .substring(0, totalMoney.length - 2)
+        .concat(".", totalMoney.slice(-2));
+    }
+
+    // Change string to float
+    totalMoney = Number.parseFloat(totalMoney);
+
+    this.setState({ totalMoney });
   }
+
   handleSubmit(e) {}
 
   render() {
@@ -58,11 +82,13 @@ class SettingsModal extends Component {
           <div className="settings-modal__total-budget">
             <label htmlFor="total-budget-input">Total Budget:</label>
             <input
-              type="text"
+              type="number"
               className="settings-modal__total-budget-input"
               id="total-budget-input"
               value={this.state.totalMoney}
               onChange={this.handleTotalMoneyInputChange}
+              min="0"
+              max="500000"
             />
             <span className="settings-modal__input-icon">$</span>
           </div>
